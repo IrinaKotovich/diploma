0cc92a734182ee9f36f3d43221375d9f8ae952de
musicaddicted <musicaddicted@astralnalog.ru>
2017-04-14 14:18:37 +0300

    печать по формату

--- js/script.js
+++ js/script.js
@@ -83,57 +83,58 @@ $.print = function(x){
 		if ($('#middlename_rod_form').val().substr(0,3)!=middlename) {error=2; $('#middlename_rod_form').addClass('error')}
 	}
 	if(error==2)  alert('Проверьте правильность написание полей с именем');
-	
+	error=0;
 	// если все поля заполнены и имена совпадают
 	if(!error){
 		if(x=='diploma'){
-			var printing_css='<style>\ span, .span{ display:block; margin-bottom:11px;font: 13.3333px Arial;}'
-			+'.diploma{background-image:url(./background.png); background-size: 100%;width:1077px;height:756px;//padding-top:70px;margin-left:5px;}'
-			+'.diploma .left, .diploma .right{width:45%; height:100%; float:left;margin-top:103px;height:653px;}'
-			+'.diploma .right{padding-left: 60px;}'
+			var printing_css='<style>\ span, .span{ display:block; margin-bottom:0.3cm;font: 13.3333px Arial;}'
+			+'.diploma{background-image:url(./background.png); background-size: 100%;width:28.5cm;height:18.2cm;//padding-top:70px;margin-left:5px;}'
+			+'.diploma .left, .diploma .right{width:45%; height:100%; float:left;margin-top:1.8cm;//height:653px;}'
+			+'.diploma .right{padding-left: 2cm;}'
 
-			+'.diploma .reg_number{margin-left: 280px;margin-top: 585px; }'
+			+'.diploma .reg_number{margin-left: 7.7cm;margin-top: 15.6cm;}'
 
-			+'.diploma .surname_dat{margin-left: 200px;}'
-			+'.diploma .name_dat{margin-left: 70px;display:inline }'
-			+'.diploma .middlename_dat,.diploma .middlename_rod{margin-left: 10px; display:inline }'
+			+'.diploma .surname_dat{margin-left: 5cm;}'
+			+'.diploma .name_dat{margin-left: 2cm;display:inline  }'
+			+'.diploma .middlename_dat,.diploma .middlename_rod{margin-left: 0.3cm; display:inline }'
 
-			+'.diploma .date1{margin-left: 150px; display:inline;white-space: pre;}'
-			+'.diploma .date2{display:inline;white-space: pre;margin-left: 64px;}'
-			+'.diploma .date3{margin-left: 172px;white-space: pre;}'
-			+'.diploma .name_inst{margin-left: 30px;line-height: 2;text-indent: 320px; padding: 0px;margin-top: -4px;    margin-bottom: 3px;}'
-			+'.diploma .for_name_cours{padding-left:30px;margin-top: -2px;margin-bottom: 7px;}'
-			+'.diploma .name_cours{display:inline;line-height: 2;text-indent: 20px;}'
-			+'.diploma .prog{display:inline;margin-left: 18px;}'
+			+'.diploma .date1{margin-left: 3.9cm; display:inline;white-space: pre;}'
+			+'.diploma .date2{display:inline;white-space: pre;margin-left: 1.8cm}'
+			+'.diploma .date3{margin-left: 4.5cm;white-space: pre;}'
+			+'.diploma .name_inst{margin-left: 0.8cm;line-height: 2;text-indent: 8.1cm; padding: 0px;margin-top: -0.1cm;    margin-bottom: 0.1cm;}'
+			+'.diploma .for_name_cours{padding-left:0.8cm;margin-top: -0.1cm;margin-bottom: 0.2cm;}'
+			+'.diploma .name_cours{display:inline;line-height: 2;text-indent: 1cm;}'
+			+'.diploma .prog{display:inline;margin-left: 0.5cm;}'
 
-			+'.diploma .surname_rod{margin-left: 140px; }'
-			+'.diploma .name_rod{margin-left: 50px;display:inline  }'
-			+'.diploma .scope{margin-left: 40px;margin-top:37px;text-transform:uppercase}'
-			+'.diploma .s_y {margin-top:222px;margin-left: 70px;}'
-			+'.diploma .sity{display:inline;padding-left:10px}'
-			+'.diploma .year{display:inline;padding-left:95px}'
+			+'.diploma .surname_rod{margin-left: 3.4cm;  }'
+			+'.diploma .name_rod{margin-left: 1.3cm;display:inline  }'
+			+'.diploma .scope{margin-left: 1cm;margin-top:0.9cm;text-transform:uppercase}'
+			+'.diploma .s_y {margin-top:5.9cm;margin-left: 1.8cm;}'
+			+'.diploma .sity{display:inline;padding-left:0.3cm}'
+			+'.diploma .year{display:inline;padding-left:2.4cm}'
 			
 			+'@media print {'
 			+'@page {size: landscape; margin:0;padding:0}}</style>';
 		}
 		else if(x=='attachment'){
 			var printing_css='<style> span, .span{ display:block; margin-bottom:0.5cm;font: 13.3333px Arial;}'
+			+'.span{display:table}'
 			+'span.number span{display:inline; margin-right:5px}'
 			
-			+'.attachment{background-image:url(background1.jpg); background-size: 100%;width:14.25cm;height:20cm;padding-top: 1.3cm;}'
+			+'.attachment{background-image:url(background1.jpg); background-size: 100%;width:14.25cm;height:20cm;padding-top: 1.3cm;margin:auto}'
 			+'.attachment .number{margin-left:11cm}'
-			+'.attachment .surname{margin-left:1.5cm;float:left; margin-top:2.2cm}'
-			+'.attachment .name, .attachment .middlename{ margin-top:2.2cm;float:left;margin-left:0.25cm;}'
+			+'.attachment .surname{margin-left:1.5cm;float:left; margin-top:2.4cm}'
+			+'.attachment .name, .attachment .middlename{ margin-top:2.4cm;float:left;margin-left:0.25cm;}'
 			
-			+'.attachment .educ{margin-top:4.5cm;margin-left:7.5cm;}'
+			+'.attachment .educ{margin-top:4.7cm;margin-left:7.5cm;}'
 			
-			+'.attachment .date1{margin-left: 1.8cm; float:left;white-space: pre;margin-top: 1cm;}'
-			+'.attachment .date2{float:left;white-space: pre;margin-left: 2.8cm;margin-top: 1cm;}'
+			+'.attachment .date1{margin-left: 1.8cm; float:left;white-space: pre;margin-top: 0.9cm;}'
+			+'.attachment .date2{float:left;white-space: pre;margin-left: 2.8cm;margin-top: 0.9cm;}'
 			
-			+'.attachment .name_inst{margin-top:-0.7cm;margin-left: 1.5cm;text-indent: 10cm;line-height: 2.5;}'
-			+'.attachment .name_cours{margin-top: -0.3cm;margin-left: 1.5cm;text-indent: 2.5cm;line-height: 1.3;}'
+			+'.attachment .name_inst{margin-top:-0.9cm;margin-left: 1.5cm;text-indent: 10cm;line-height: 2.5}'
+			+'.attachment .name_cours{margin-top: 0.2cm;margin-left: 1.5cm;text-indent: 2.5cm;line-height: 1.3;}'
 			
-			+'.attachment .atestation{margin-top: 2.5cm;margin-left: 1.5cm;text-indent: 8.5cm;line-height: 1.3;}'
+			+'.attachment .atestation{margin-top: 2.3cm;margin-left: 1.5cm;text-indent: 8.5cm;line-height: 1.3;}'
 			+'.attachment .evaluation{display:inline}'
 			
 			+'@media print {'
@@ -141,9 +142,9 @@ $.print = function(x){
 		}
 		else if(x=='attachment2'){
 			var printing_css='<style>select, span, .span{ display:block; margin-bottom:11px}'
-			+'table{font: 13.3333px Arial;color:green}'
+			+'table{font: 13.3333px Arial;}'
 			
-			+'.attachment2{background-image:url(background2.jpg); background-size: 100%;width:14.25cm;height:20cm;padding-top: 2.5cm;float:left;}'
+			+'.attachment2{background-image:url(background2.jpg); background-size: 100%;width:14.25cm;height:20cm;padding-top: 2.5cm;margin:auto}'
 			+'.attachment2 .for_attachment2{    margin-top: 1cm;width: 12cm;margin-left: 1cm;}'
 			+'.attachment2 .for_attachment2 tr:nth-child(1) {display:none}'
 			+'.attachment2 .for_attachment2 tr td:nth-child(1){width: 0.9cm;vertical-align: top;padding-top: 9px;text-align: center;}'
@@ -152,7 +153,7 @@ $.print = function(x){
 			+'.attachment2 .for_attachment2 tr td:nth-child(4){text-align: center;}'
 	
 			+'.attachment2 .for_attachment2 select{border:none; background:none;padding-left: 0.8cm;-webkit-appearance: none;-moz-appearance: none}'
-			+'.attachment2 .hours{margin-top:2.8cm; margin-left:4cm;color:green}'
+			+'.attachment2 .hours{margin-top:2.8cm; margin-left:4cm;}'
 			+'@media print {'
 			+'@page { margin:0;padding:0}'
 	
